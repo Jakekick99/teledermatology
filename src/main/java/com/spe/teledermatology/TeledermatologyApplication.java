302adb9d599e5636fb2810f39c86a689bad1946e
Jakekick99 <jakekallarackal@gmail.com>
2023-04-11 12:43:58 +0530

    Initial commit

---
+++ src/main/java/com/spe/teledermatology/TeledermatologyApplication.java
@@ -0,0 +1,13 @@
+package com.spe.teledermatology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeledermatologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeledermatologyApplication.class, args);
+	}
+
+}
